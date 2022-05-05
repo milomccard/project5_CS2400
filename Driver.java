722762972da82daa50ad9a964024504d7662124f
Milo McCard <milo.mccard01@gmail.com>
2022-05-05 15:35:23 -0700

    added queue

--- Driver.java
+++ Driver.java
@@ -3,9 +3,33 @@ public class Driver {
 
     }
 
-    public static T breadthFirst(int originVertex){
+    public static QueueInterface<T> breadthFirst(T origin){
+        resetVertices();
+        QueueInterface<T> traversalOrder = new LinkedQueue<T>();
+        QueueInterface<VertexInterface<T>> vertexQueue = new LinkedQueue<VertexInterface<T>>();
 
-    }
+        VertexInterface<T> originVertex = vertices.getValue(origin);
+        originVertex.visit();
+        traversalOrder.enqueue(origin);
+        vertexQueue.enqueue(originVertex);
+
+        while (neighbors.hasNext()){
+            VetexInterface<T> frontVertex = vertexQueue.dequeue();
+
+            Iterator<VertexInterface<T>> neighbors =
+                                            frontVertex.getNeighborIterator();
+            
+            while (neighbors.hasNext()){
+                VertexInterface<T> nextNeighbor = neighbors.next();
+                if (!nextNeighbor.isVisited()){
+                    nextNeighbor.visit():
+                    traversalOrder.enqueue(nextNeighbor.getLabel());
+                    vertexQueue.enqueue(nextNeighbor);
+                }
+            }
+        }
+        return traversalOrder;
+        }
 
     public static T depthFirst(){
 
