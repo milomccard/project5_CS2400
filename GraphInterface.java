0173e96e16e805fcccd9c43d4b609f5f4a2c2a89
milomccard <98611536+milomccard@users.noreply.github.com>
2022-05-09 10:16:04 -0700

    Update GraphInterface.java

--- GraphInterface.java
+++ GraphInterface.java
@@ -1,9 +1,14 @@
 public interface GraphInterface<T> {
 
+  /**
+  * Accepts the number of the vertex and returns the object held in the memory location
+  * @param vertex is the integer value of a specific vertex
+  * @retruns T an object held in a specific memory location
+  **/
   public T getLabel(int vertex);
 
   public boolean isEdge(int source, int target);
-
+  
   public void addEdge(int source, int target);
 
   public int[] neighbors(int vertex);
